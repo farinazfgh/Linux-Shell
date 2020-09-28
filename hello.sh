ca3fdc7fbd243e01e709335929e9f88d55dbd105
farinaz-ghasemi-wcar <farinaz.ghasemi@wirelesscar.com>
2020-09-28 17:23:04 +0200

    hello bash7

---
+++ hello.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Usage: Hello World Bash Shell Script Using Variables
+# -------------------------------------------------
+ 
+# Define bash shell variable called var 
+# Avoid spaces around the assignment operator (=)
+var="HELLO"
+ 
+# print it 
+echo "$var"
+ 
+# Another way of printing it
+#printf "%s\n" "$var"
